b6a2d396971cd40e91bb330537d8c26926b4db7a
Masud8851 <masud1971.cse@gmail.com>
2022-02-04 00:38:51 +0600

    Basic JS

--- ProblemSolving/Basic/function.js
+++ ProblemSolving/Basic/function.js
@@ -2,3 +2,19 @@ function functionName() {
 	console.log("Function Calling");
 }
 functionName();
+
+function addition(a, b) {
+	var sum = a + b;
+	return sum;
+}
+console.log(addition(4, 6));
+
+function callMyName(name, callback) {
+	var myAge = 23;
+	callback(myAge);
+	console.log("Is it interesting? Yes it is Mr." + name);
+}
+function hello(age) {
+	console.log("I am passed through argument and my age is: " + age);
+}
+callMyName("Masudur Rahman", hello);
